d483d12d7e3b4bdcdf40ab30ae70125bc542d45c
Eduardo Borba <eduardo@jobscore.com>
2018-04-04 16:09:49 -0300

    Use postgresql instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180217234627) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bet_leagues", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false

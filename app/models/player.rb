9815704f04e55b7eb30879ce09a6e293ea1bf54d
Eduardo Borba <eduardo@jobscore.com>
2019-04-09 08:30:46 -0300

    Add devise gem and configure it

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,18 @@
 class Player < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   has_many :player_bet_leagues
   has_many :bet_leagues, through: :player_bet_leagues
   has_many :bets
 
   validates :name, :email, :password, presence: true
+
+  # before_validation :set_provider
+
+  # def set_provider
+  #   self[:provider] = "email" if self[:provider].blank?
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'Player', at: 'auth'
   resources :rounds
   resources :teams
   resources :championships
